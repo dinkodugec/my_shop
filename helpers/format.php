d7388b1d28805c71eec79782314fb16ba605e886
Dinko <dinko.dugec@gmail.com>
2021-11-14 22:56:58 +0100

    product list

--- helpers/format.php
+++ helpers/format.php
@@ -10,6 +10,16 @@ class Format
         return $data;
 
     }
+
+    public function textShorten($text, $limit = 400)
+    {
+        $text = $text. "";
+        $text = substr($text, 0, $limit);
+        $text = $text."..";
+        return $text;
+    
+    
+    }
     
 }
 
